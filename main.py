07f2c930f577f6da946e95954810b7f515d99b22
Walkoszes <130684485+Walkoszes@users.noreply.github.com>
2023-05-13 18:40:18 +0300

    Add files via upload
    Added the button with clearing so you won't need to close it evetime. Also added the explanation of the BMI after the button "What does it mean" was pressed, the explanation appear below the button

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ window = QWidget()
 window.setWindowTitle("Definition of BMI")
 window.resize(459,213)
 
-weight = QLabel("Body weight:")
-height = QLabel("Body height:")
+weight = QLabel("Body weight in kg:")
+height = QLabel("Body height in cm:")
 result = QLabel("Your BMI = ")
+explanation = QLabel()
 button = QPushButton("Find out your BMI")
 button2 = QPushButton("What does it mean")
+button3 = QPushButton("Clear")
 wrt_weight = QLineEdit()
 wrt_height = QLineEdit()
 
@@ -26,10 +28,33 @@ def equaling():
    return str(equal)
 
 def cal_bmi():
+   global result1
    result1 = equaling()
    result.setText("Your BMI = " + result1)
 
 button.clicked.connect(cal_bmi)
+
+def clearing():
+   wrt_weight.clear()
+   wrt_height.clear()
+   result.setText("Your BMI = ")
+   explanation.clear()
+
+button3.clicked.connect(clearing)
+
+def table_expl():
+   if int(result1) >= 1 and int(result1) <=15:
+      explanation.setText("Low, you have dystrophy, very low weight")
+   if int(result1) >=16 and int(result1) <=18:
+      explanation.setText("Average, deviation from the norm, but within the limits of age changes: you grow faster than you gain weight")
+   if int(result1) >= 18.5 and int(result1) <= 24.9:
+      explanation.setText("Normal, your weight is normal")
+   if int(result1) >=25 and int(result1) <= 30:
+      explanation.setText("Average, deviation from the norm, but within the limits of age changes: you gain weight faster than you grow")
+   if int(result1) >= 30 and int(result1) <= 50:
+      explanation.setText("High, you are obese, very heavy")
+
+button2.clicked.connect(table_expl)
 
 row1 = QHBoxLayout()
 row2 = QHBoxLayout()
@@ -47,8 +72,10 @@ col2.addWidget(wrt_weight)
 col3.addWidget(height)
 col5.addWidget(wrt_height)
 col4.addWidget(button)
+col4.addWidget(button3)
 col4.addWidget(result)
 col4.addWidget(button2)
+col4.addWidget(explanation)
 
 row1.addLayout(col1)
 row1.addLayout(col2)
